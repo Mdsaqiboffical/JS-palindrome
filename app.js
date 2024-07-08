aa6876245490d4542fc6ecf01799229710dc2dbe
Mdsaqiboffical <154467672+Mdsaqiboffical@users.noreply.github.com>
2024-07-09 00:59:44 +0500

    done2

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 function palindromeCheck() {
+    // get the id
     let userInput = document.getElementById("user-id").value;
+    // split the string
     let splitMethod = userInput.split("");
+    // Reverse the string
     let reverseMethod = splitMethod.reverse();
+    // join the string
     let joinMethod = reverseMethod.join("");
-
+    // condition
     if (userInput === "") {
         alert("invalid")
     } if (joinMethod === userInput) {
-        // alert(userInput + " Is Palindrome")
         let newid = document.getElementById("check-input").innerText = userInput + " Is palindrome";
     } else {
-        // alert(userInput + " Is Not Palindrome")
         let newid = document.getElementById("check-input").innerText = userInput + " Is not palindrome";
-        // userInput2 + " is not palindrome";
     }
+    // 
     // console.log(userInput, splitMethod, reverseMethod, joinMethod);
 }
